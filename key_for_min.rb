df972590bc087803f8b0b489013783e11ea17486
Aaron Amodt <aaron@amodttech.com>
2020-10-18 01:18:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,13 +4,11 @@
 def key_for_min_value(name_hash)
 
 lowest = 100
-lowest_key = "nothing yet"
+lowest_key = nil
 name_hash.each do |key, value|
   if value < lowest
     lowest = value
     lowest_key = key
-    elsif value == nil 
-      lowest_key = nil
   end
 end
   lowest_key
